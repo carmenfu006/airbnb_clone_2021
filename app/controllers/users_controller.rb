297aa086784d2144b4fb9c2ab6eaaf3f33808d49
carmenfu006 <guess_006@hotmail.com>
2021-06-17 01:10:44 +0800

    Setup user profile and setting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,26 @@
 class UsersController < ApplicationController
   def show
-    
   end
+
+  def edit
+  end
+
+  def update
+    if current_user&.valid_password?(params[:user][:password])
+      current_user.update(user_params)
+      
+      if current_user.save
+        redirect_to user_path(current_user), notice: 'User profile was successfully updated.'
+      else
+        render :edit
+      end
+    else
+      redirect_to edit_user_path(current_user), alert: 'Password incorrect'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:fullname, :username, :birth_date, :avatar)
+    end
 end
